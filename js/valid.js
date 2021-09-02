7a06a58e1b1e2e572a0d6adf365b9f0ee34625e4
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-09-02 14:05:54 +0200

    Fix linter errors

--- js/valid.js
+++ js/valid.js
@@ -10,12 +10,12 @@ small.style.padding = '12px';
 small.style.visibility = 'hidden';
 
 form.addEventListener('submit', (e) => {
-    if (email.value !== email.value.toLowerCase()) {
-        small.style.visibility = 'visible';
-        email.style.border = '1px solid #e74c3c';
-        e.preventDefault();
-    } else{
-        small.style.visibility = 'hidden';
-        email.style.border = '1px solid #d0d9d4';
-    }
-})
+  if (email.value !== email.value.toLowerCase()) {
+    small.style.visibility = 'visible';
+    email.style.border = '1px solid #e74c3c';
+    e.preventDefault();
+  } else {
+    small.style.visibility = 'hidden';
+    email.style.border = '1px solid #d0d9d4';
+  }
+});
