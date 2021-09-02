a30e4e64b353bd0988b97e979756902c581c74fb
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-09-02 12:58:42 +0200

    Create alert message and style properties from valid.js script

--- js/valid.js
+++ js/valid.js
@@ -1,12 +1,21 @@
 const form = document.getElementById('form');
 const email = document.getElementById('mail');
-const small = document.querySelector('.alert');
+const alertParent = document.querySelector('.btn-submit-alert');
+const small = document.createElement('small');
+alertParent.appendChild(small);
+small.innerText = 'Email field has to be in lower case.';
+small.style.color = '#fff';
+small.style.backgroundColor = '#e74c3c';
+small.style.padding = '12px';
+small.style.visibility = 'hidden';
 
 form.addEventListener('submit', (e) => {
     if (email.value !== email.value.toLowerCase()) {
         small.style.visibility = 'visible';
+        email.style.border = '1px solid #e74c3c';
         e.preventDefault();
     } else{
         small.style.visibility = 'hidden';
+        email.style.border = '1px solid #d0d9d4';
     }
 })
