0cb6db1122916b2f07152c783ee0041d69ec92fb
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-09-02 17:18:06 +0200

    Change the alert message color and the input email border

--- js/valid.js
+++ js/valid.js
@@ -1,25 +1,30 @@
 const form = document.getElementById('form');
 const email = document.getElementById('mail');
 const alertParent = document.querySelector('.btn-submit-alert');
-const small = document.createElement('small');
-alertParent.appendChild(small);
-small.innerText = 'Please enter a valid email address.';
-small.style.backgroundColor = '#fff';
-small.style.color = '#e74c3c';
-small.style.padding = '12px';
-small.style.visibility = 'hidden';
+const alertMsg = document.createElement('p');
+alertParent.appendChild(alertMsg);
+alertMsg.innerText = 'Please enter a valid email address.';
+alertMsg.style.backgroundColor = '#fff';
+alertMsg.style.color = '#36b37e';
+alertMsg.style.fontWeight = '600';
+alertMsg.style.textShadow = '1px 1px 5px #36b37e';
+alertMsg.style.padding = '12px';
+alertMsg.style.visibility = 'hidden';
 
 form.addEventListener('submit', (e) => {
   if (email.value !== email.value.toLowerCase()) {
-    small.style.visibility = 'visible';
+    alertMsg.style.visibility = 'visible';
+    email.style.border = ' 2px solid rgba(1, 155, 86, 0.945)';
+    email.style.boxShadow = '1px 1px 10px rgb(7 176 100 / 73%)';
+
     setTimeout(() => {
-      small.style.visibility = 'hidden';
+      alertMsg.style.visibility = 'hidden';
       email.style.border = '1px solid #d0d9d4';
+      email.style.boxShadow = 'none';
     }, 8000);
-    email.style.border = '1px solid #e74c3c';
     e.preventDefault();
   } else {
-    small.style.visibility = 'hidden';
+    alertMsg.style.visibility = 'hidden';
     email.style.border = '1px solid #d0d9d4';
   }
 });
