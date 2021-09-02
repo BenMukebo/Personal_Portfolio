faf4519c466e619487440200280198ca8a7fc258
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-09-02 13:57:21 +0200

    Change the validation message

--- js/valid.js
+++ js/valid.js
@@ -3,7 +3,7 @@ const email = document.getElementById('mail');
 const alertParent = document.querySelector('.btn-submit-alert');
 const small = document.createElement('small');
 alertParent.appendChild(small);
-small.innerText = 'Email field has to be in lower case.';
+small.innerText = 'Please enter a valid email address.';
 small.style.color = '#fff';
 small.style.backgroundColor = '#e74c3c';
 small.style.padding = '12px';
